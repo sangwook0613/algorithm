bd7c38744d0642ac04a78115d158c12c13ab1b95
sangwook0613 <kvmoke@gmail.com>
2021-03-05 23:32:36 +0900

    Solved problems 0305

--- Beakjoon/2206_벽부수고이동하기.py
+++ Beakjoon/2206_벽부수고이동하기.py
@@ -1,49 +1,43 @@
-# from collections import deque
-#
-# dx = [-1, 1, 0, 0]
-# dy = [0, 0, -1, 1]
-#
-#
-# def bfs(a, b):
-#     queue = deque()
-#     queue.append((a, b))
-#     while queue:
-#         x, y = queue.popleft()
-#         for k in range(4):
-#             nx = x + dx[k]
-#             ny = y + dy[k]
-#             if nx < 0 or nx > N-1 or ny < 0 or ny > M-1 or board[nx][ny] != 0:
-#                 continue
-#             if board[nx][ny] == 0 and visited[nx][ny] == 0:
-#                 visited[nx][ny] = visited[x][y] + 1
-#                 queue.append((nx, ny))
-#     if visited[N-1][M-1] == 0:
-#         return 1000000
-#     else:
-#         return visited[N-1][M-1] + 1
-#
-#
-# N, M = map(int, input().split())
-# board = [list(map(int, input())) for _ in range(N)]
-# ans = 1000000
-# for i in range(N):
-#     for j in range(M):
-#         if board[i][j] == 1:
-#             chk = 0
-#             for c in range(4):
-#                 ci = i + dx[c]
-#                 cj = j + dy[c]
-#                 if 0 <= ci <= N-1 and 0 <= cj <= M-1 and board[ci][cj] == 1:
-#                     chk += 1
-#             if chk >= 3:
-#                 continue
-#             visited = [[0]*M for _ in range(N)]
-#             board[i][j] = 0
-#             cnt = bfs(0, 0)
-#             if ans > cnt:
-#                 ans = cnt
-#             board[i][j] = 1
-# if ans == 1000000:
-#     print(-1)
-# else:
-#     print(ans)
+from collections import deque
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(a, b):
+    queue = deque()
+    queue.append((a, b, 0))
+    visited[0][a][b] = 1
+    visited[1][a][b] = 1
+    while queue:
+        x, y, jump = queue.popleft()
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if nx < 0 or nx > N-1 or ny < 0 or ny > M-1:
+                continue
+            if jump == 0:
+                if board[nx][ny] == 0 and visited[jump][nx][ny] == 0:
+                    visited[jump][nx][ny] = visited[jump][x][y] + 1
+                    queue.append((nx, ny, 0))
+                if board[nx][ny] == 1 and visited[1][nx][ny] == 0:
+                    visited[1][nx][ny] = visited[0][x][y] + 1
+                    queue.append((nx, ny, 1))
+            else:
+                if board[nx][ny] == 0 and visited[jump][nx][ny] == 0:
+                    visited[jump][nx][ny] = visited[jump][x][y] + 1
+                    queue.append((nx, ny, 1))
+    if visited[0][N-1][M-1] == 0 and visited[1][N-1][M-1] == 0:
+        return -1
+    else:
+        if visited[0][N-1][M-1] == 0:
+            return visited[1][N-1][M-1]
+        if visited[1][N-1][M-1] == 0:
+            return visited[0][N-1][M-1]
+        return min(visited[0][N-1][M-1], visited[1][N-1][M-1])
+
+
+N, M = map(int, input().split())
+board = [list(map(int, input())) for _ in range(N)]
+visited = [[[0]*M for _ in range(N)]]
+visited += [[[0]*M for _ in range(N)]]
+print(bfs(0, 0))

aba7586ea80422e67a3357305665a23e75b7ae18
sangwook0613 <kvmoke@gmail.com>
2021-03-12 23:57:59 +0900

    Solved problems 0312

--- SWEA/1949_등산로조성.py
+++ SWEA/1949_등산로조성.py
@@ -2,10 +2,6 @@
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def print_for(arr):
-    for n in range(N):
-        print(arr[n])
-    print()
 
 def get_max(arr):
     max_num = 0
@@ -20,8 +16,6 @@ def dfs(a, b, d, cnt, visited):
     global ans
     if cnt > ans:
         ans = cnt
-    # print(a, b, cnt, d)
-    # print_for(visited)
     visited[a][b] = 1
     for k in range(4):
         nx = a + dx[k]
@@ -33,13 +27,18 @@ def dfs(a, b, d, cnt, visited):
                 visited[nx][ny] = 1
                 dfs(nx, ny, d, cnt+1, visited)
                 visited[nx][ny] = 0
-            if mountain[nx][ny] >= mountain[a][b] and d != 0 and (mountain[nx][ny] - mountain[a][b]) <= d-1 :
+            if mountain[nx][ny] >= mountain[a][b] and d != 0 and (mountain[nx][ny] - mountain[a][b]) <= d-1:
+                if mountain[nx][ny] - ((mountain[nx][ny] - mountain[a][b]) + 1) < 0:
+                    continue
                 d = 0
                 visited[nx][ny] = 1
-                mountain[nx][ny] -= 1
+                temp = mountain[nx][ny]
+                mountain[nx][ny] = mountain[a][b] - 1
                 dfs(nx, ny, d, cnt + 1, visited)
-                mountain[nx][ny] += 1
+                mountain[nx][ny] = temp
                 visited[nx][ny] = 0
+                d = K
+
 
 T = int(input())
 
@@ -52,9 +51,7 @@ for t in range(1, T+1):
     for i in range(N):
         for j in range(N):
             if mountain[i][j] == max_height:
-                # print(i, j)
                 dfs(i, j, K, 0, chk)
                 chk[i][j] = 0
-                # print(ans)
 
-    print(ans+1)
+    print('#%d %d' % (t, ans+1))
---
+++ SWEA/1953_탈주범검거.py
@@ -0,0 +1,64 @@
+dxy = [[],
+       [(-1, 0), (1, 0), (0, -1), (0, 1)],
+       [(-1, 0), (1, 0), (0, 0), (0, 0)],
+       [(0, 0), (0, 0), (0, -1), (0, 1)],
+       [(-1, 0), (0, 0), (0, 0), (0, 1)],
+       [(0, 0), (1, 0), (0, 0), (0, 1)],
+       [(0, 0), (1, 0), (0, -1), (0, 0)],
+       [(-1, 0), (0, 0), (0, -1), (0, 0)]]
+
+
+pipe = [[1, 2, 5, 6],
+        [1, 2, 4, 7],
+        [1, 3, 4, 5],
+        [1, 3, 6, 7]
+        ]
+
+
+def print_for(arr):
+    for i in arr:
+        print(i)
+    print()
+
+
+def get_sum(arr, n, m):
+    cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == 1:
+                cnt += 1
+    return cnt
+
+
+def bfs(a, b, h):
+    queue = []
+    queue.append((a, b, h))
+    visited[a][b] = 1
+    while queue:
+        x, y, depth = queue.pop(-1)
+        if depth == 0:
+            continue
+        for k in range(len(dxy[tunnel[x][y]])):
+            if dxy[tunnel[x][y]][k][0] == 0 and dxy[tunnel[x][y]][k][1] == 0:
+                continue
+            nx = x + dxy[tunnel[x][y]][k][0]
+            ny = y + dxy[tunnel[x][y]][k][1]
+            if nx < 0 or nx >= N or ny < 0 or ny >= M:
+                continue
+            if tunnel[nx][ny] in pipe[k] and not visited[nx][ny]:
+                visited[nx][ny] = 1
+                queue.append((nx, ny, depth - 1))
+
+
+
+T = int(input())
+
+for t in range(1, T+1):
+    N, M, start_r, start_c, hour = map(int, input().split())
+    tunnel = [list(map(int, input().split())) for _ in range(N)]
+    visited = [[0]*M for _ in range(N)]
+    # print_for(visited)
+    bfs(start_r, start_c, hour - 1)
+    # print_for(visited)
+
+    print('#%d %d' % (t, get_sum(visited, N, M)))

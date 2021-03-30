43d2635d96f4c2eb8ee5b40c1a6986c3cc2dd716
sangwook0613 <kvmoke@gmail.com>
2021-03-30 23:53:49 +0900

    Update solved problem 0330

--- Beakjoon/16922_로마숫자만들기.py
+++ Beakjoon/16922_로마숫자만들기.py
@@ -2,16 +2,16 @@ base_num = [1, 5, 10, 50]
 result = []
 
 
-def solve(total, cnt, n):
+def solve(total, cnt, k, n):
     if cnt == n:
         global result
         if total not in result:
             result.append(total)
         return
-    for i in range(4):
-        solve(total + base_num[i], cnt+1, n)
+    for i in range(k, 4):
+        solve(total + base_num[i], cnt+1, i, n)
 
 
 N = int(input())
-solve(0, 0, N)
+solve(0, 0, 0, N)
 print(len(result))

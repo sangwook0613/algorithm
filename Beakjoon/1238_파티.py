7b12c17529f3d468192f28ebd2eb26cf6323393e
sangwook0613 <kvmoke@gmail.com>
2021-04-21 23:56:49 +0900

    Solved problem 0421

---
+++ Beakjoon/1238_파티.py
@@ -0,0 +1,35 @@
+def dijkstra(start):
+    visited = [0]*(N+1)
+    weight = [0] + [987654321]*N
+    visited[start] = 1
+    weight[start] = 0
+
+    for n, w in nodes[start]:
+        weight[n] = w
+
+    for _ in range(1, N):
+        # print([weight[i] for i in range(1, N+1) if not visited[i]])
+        min_idx = weight.index(min([weight[i] for i in range(1, N+1) if not visited[i]]))
+        # print(min_idx)
+        visited[min_idx] = 1
+        for n, w in nodes[min_idx]:
+            weight[n] = min(weight[n], weight[min_idx] + w)
+
+    return weight
+
+
+N, M, X = map(int, input().split())
+
+nodes = [[] for _ in range(N+1)]
+for _ in range(M):
+    start, end, weight = map(int, input().split())
+    nodes[start].append((end, weight))
+
+print(nodes)
+result = dijkstra(X)
+for k in range(1, N+1):
+    temp = dijkstra(k)
+    print(k, temp)
+    result[k] += temp[X]
+
+print(max(result))

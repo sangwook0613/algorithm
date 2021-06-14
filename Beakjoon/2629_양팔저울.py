1516b1e99e042ea9516a9f6a659bf5f9c10e59bc
sangwook0613 <kvmoke@gmail.com>
2021-06-14 23:46:06 +0900

    Solved problem 0614

--- Beakjoon/2629_양팔저울.py
+++ Beakjoon/2629_양팔저울.py
@@ -1,5 +1,6 @@
+## 풀이1
 def solve(lev, total):
-    if memo[lev][total] or total > 15000:
+    if memo[lev][total]:
         return
     memo[lev][total] = 1
     if lev == N:
@@ -15,9 +16,42 @@ weight = list(map(int, input().split()))
 
 C = int(input())
 chk_weight = list(map(int, input().split()))
-memo = [[0]*30000 for _ in range(30)]
+memo = [[0 for _ in range(80001)] for __ in range(N + 1)]
 
 solve(0, 0)
+
+ans = []
+for i in chk_weight:
+    if memo[N][i]:
+        ans.append('Y')
+    else:
+        ans.append('N')
+
+print(' '.join(ans))
+
+
+## 풀이2
+def solve(lev, left, right):
+    total = abs(left - right)
+    if memo[lev][total]:
+        return
+    memo[lev][total] = 1
+    if lev == N:
+        return
+
+    solve(lev + 1, left + weight[lev], right)
+    solve(lev + 1, left, right + weight[lev])
+    solve(lev + 1, left, right)
+
+
+N = int(input())
+weight = list(map(int, input().split()))
+
+C = int(input())
+chk_weight = list(map(int, input().split()))
+memo = [[0 for _ in range(40001)] for __ in range(N + 1)]
+
+solve(0, 0, 0)
 
 ans = []
 for i in chk_weight:

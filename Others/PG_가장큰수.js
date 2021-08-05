9109cd8ffcaaea95152144731c22e3453769ed7c
ty1295 <ty1295@naver.com>
2021-08-05 23:56:00 +0900

    Solved problem 0805

--- Others/PG_가장큰수.js
+++ Others/PG_가장큰수.js
@@ -1,40 +1,23 @@
+// 프로그래머스 가장 큰 수
+// 문자열 정렬의 특징을 잘 이해하자!
+// 문자열로 합해서 비교하는 것이 핵심!!!
+// 0 만 있는 예외 케이스도 항상 고려하자!
+
+
 function solution(numbers) {
-  var answer = '';
-  
-  // 끝에 0이 있는지 없는지 파악해서 정렬
-  
-  function setOrder(a, b) {
-      let len = (a.length > b.length) ? a.length : b.length
-      for (let i = 0; i < len; i++) {
-          if (a[i] > b[i]) {
-              return a
-          }
-      }
-  }
-  setOrder('123', '1123')
-  
-  
-//     function setOrder(a, b) {
-//         if (a < b) {
-//             return 1
-//         } else if (a > b) {
-//             return -1
-//         } else {
-//             return 0
-//         }
-//     }
-  
-  let arr = []
-  let idx = []
-  for (let i in numbers) {
-      if (numbers[i] % 10 === 0) {
-          
-      }
-      arr.push(String(num))
-  }
-  arr.sort(setOrder)
-  for (let n of arr) {
-      answer += n
-  }
-  return answer;
+    var answer = '';
+    let arr = []
+    for (let i in numbers) {
+        arr.push(String(numbers[i]))
+    }
+    
+    arr.sort(((a, b) => (b+a) - (a+b)))
+    
+    for (let n of arr) {
+        answer += n
+    }
+    if (parseInt(answer) === 0) {
+        return '0'
+    }
+    return answer;
 }

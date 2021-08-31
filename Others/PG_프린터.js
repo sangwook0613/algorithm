ee824dc1d902d0a6b9e83432bbcc8d15de634215
sangwook0613 <kvmoke@gmail.com>
2021-08-31 23:38:14 +0900

    Solved problem 0831

--- Others/PG_프린터.js
+++ Others/PG_프린터.js
@@ -1,33 +1,32 @@
+// 프로그래머스 프린터
+// 큐를 응용한 문제, js에서 인덱스와 값을 접근하는 방식을 잘 익히자!
+
 function solution(priorities, location) {
-  var answer = 0;
-  let order = []
-  let deleteNums = []
-  let sorted = Array.from(priorities)
-  sorted.sort((a, b) => b-a)
-  let idx = 0
-  let p = 0
-  let total = sorted.reduce((a, b) => a+b)
-  console.log('total', total)
-  while (total > 0) {
-      if (p === priorities.length) {
-          console.log(p)
-          for (let d of deleteNums) {
-              total -= priorities[d]
-              priorities.splice(d, 1, 0)
-          }
-          console.log(total, deleteNums)
-          deleteNums = []
-          break
-          p = 0
-      }
-      if (priorities[p] === sorted[idx]) {
-          order.push(p)
-          sorted.shift()
-          deleteNums.push(p)
-          idx++
-          console.log(order, sorted, priorities, idx)
-      }
-      p++
-  }
-  return answer;
+    let paper = []
+    for (let i in priorities) {
+        paper.push([parseInt(i), priorities[i]])
+    }
+    let answer = []
+    
+    while (paper.length > 0) {
+        let temp = []
+        for (let p of paper) {
+            temp.push(p[1])
+        }
+        let current = paper.shift()
+        
+        if (current[1] >= Math.max(...temp)) {
+            answer.push(current[0])
+        } else {
+            paper.push(current)
+        }
+    }
+    
+    let ans = 1
+    for (let a of answer) {
+        if (a == location) {
+            return ans
+        }
+        ans++
+    }
 }

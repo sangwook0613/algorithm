bbce9bb576f90b2fbfa8ed65b4a56fcf648e1025
sangwook0613 <kvmoke@gmail.com>
2021-09-20 23:52:19 +0900

    Solved problem 0920

--- Beakjoon/11048_이동하기.py
+++ Beakjoon/11048_이동하기.py
@@ -2,25 +2,12 @@
 ## BFS 인줄 알았으나 3의 1000제곱이기에 시간초과가 난다
 ## DP로 다시 풀어보자
 
-from collections import deque
-dxy = [(0, 1), (1, 0), (1, 1)]
-
-
-def bfs(i, j, k):
-    q = deque([(i, j, k)])
-
-    while q:
-        x, y, cur = q.popleft(0)
-        for dx, dy in dxy:
-            a = x + dx
-            b = y + dy
-            if 0 <= a < N and 0 <= b < M:
-                count[a][b] = max(count[a][b], board[a][b]+cur)
-                q.append((a, b, count[a][b]))
-
-
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-count = [[0]*M for _ in range(N)]
-bfs(0, 0, board[0][0])
-print(count[N-1][M-1])
+dp = [[0]*(M+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, M+1):
+        dp[i][j] = board[i-1][j-1] + max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+
+print(dp[N][M])

254421569fc0020dfe9506a77c0fa4217aedb935
sangwook0613 <kvmoke@gmail.com>
2021-10-09 23:50:47 +0900

    Solving problem 1009

--- Others/PG_전력망을둘로나누기.py
+++ Others/PG_전력망을둘로나누기.py
@@ -1,16 +1,36 @@
 # 프로그래머스 전력망을 둘로 나누기
-## 천천히~~
+## 유니온파인드로 풀었는데도 아니다.. 왜??
+
+def find(tower, a):
+    while tower[a] != a:
+        a = tower[a]
+    return a
+
+
+def union(tower, a, b):
+    x = find(tower, a)
+    y = find(tower, b)
+    if x > y:
+        tower[x] = y
+    else:
+        tower[y] = x
+
 
 def solution(n, wires):
-    answer = -1
-    cnt = [1]*(n+1)
-    parent = [[] for _ in range(n+1)]
-    for a, b in wires:
-        parent[a].append(b)
-    print(parent)
-    for i in range(n, 0, -1):
-        if len(parent[i]) != 0:
-            for k in parent[i]:
-                cnt[i] += cnt[k]
-    print(cnt)
+    answer = 1000
+    for i in range(len(wires)):
+        tower = [i for i in range(n + 1)]
+        for j in range(len(wires)):
+            if j == i:
+                continue
+            union(tower, wires[j][0], wires[j][1])
+
+        num = [[tower[1], 1], [0, 0]]
+        for k in range(2, n + 1):
+            if tower[k] == num[0][0]:
+                num[0][1] += 1
+            else:
+                num[1][0] = tower[k]
+                num[1][1] += 1
+        answer = min(answer, abs(num[0][1] - num[1][1]))
     return answer

02c87f790e1a3cb7bd214480cbc99de9aeed4c25
sangwook0613 <kvmoke@gmail.com>
2021-10-10 21:40:03 +0900

    Solved problem 1010

--- Others/PG_전력망을둘로나누기.py
+++ Others/PG_전력망을둘로나누기.py
@@ -1,36 +1,32 @@
 # 프로그래머스 전력망을 둘로 나누기
 ## 유니온파인드로 풀었는데도 아니다.. 왜??
+## 유니온파인드에서 추가적으로 부모 노드를 맞추는 과정이 필요한것 같다
+## 우선 더 단순한 BFS로 풀었다. 원리는 같다!
 
-def find(tower, a):
-    while tower[a] != a:
-        a = tower[a]
-    return a
-
-
-def union(tower, a, b):
-    x = find(tower, a)
-    y = find(tower, b)
-    if x > y:
-        tower[x] = y
-    else:
-        tower[y] = x
+def bfs(visited, connections, start):
+    cnt = 0
+    q = [start]
+    visited[start] = 1
+    while q:
+        k = q.pop(0)
+        cnt += 1
+        for n in connections[k]:
+            if not visited[n]:
+                visited[n] = 1
+                q.append(n)
+    return cnt
 
 
 def solution(n, wires):
     answer = 1000
     for i in range(len(wires)):
-        tower = [i for i in range(n + 1)]
-        for j in range(len(wires)):
-            if j == i:
-                continue
-            union(tower, wires[j][0], wires[j][1])
+        visited = [0] * (n + 1)
+        temp = wires[:i] + wires[i + 1:]
+        connections = [[] for _ in range(n + 1)]
+        for a, b in temp:
+            connections[a].append(b)
+            connections[b].append(a)
+        result = bfs(visited, connections, wires[i][0])
+        answer = min(answer, abs(result - (n - result)))
 
-        num = [[tower[1], 1], [0, 0]]
-        for k in range(2, n + 1):
-            if tower[k] == num[0][0]:
-                num[0][1] += 1
-            else:
-                num[1][0] = tower[k]
-                num[1][1] += 1
-        answer = min(answer, abs(num[0][1] - num[1][1]))
     return answer

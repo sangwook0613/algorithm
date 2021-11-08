76628e93416bc3c9f96d6bce507749ff995001c9
sangwook0613 <kvmoke@gmail.com>
2021-11-09 00:00:39 +0900

    Solved problem 1109

--- Others/PG_다트게임.js
+++ Others/PG_다트게임.js
@@ -1,17 +1,17 @@
 // 프로그래머스 다트 게임
-
+// 하나하나 if문으로 처리
 function solution(dartResult) {
   let result = dartResult.split('')
   let num = [0, 0, 0]
   let prev = 'a'
-  let idx = 0
-  console.log(result)
+  let idx = -1
+  
   for (let s of result) {
       if (isNaN(parseInt(s))) {
           if (s === 'D') {
-              num[idx] = (idx+1)**2
+              num[idx] = num[idx]**2
           } else if (s === 'T') {
-              num[idx] = (idx+1)**3
+              num[idx] = num[idx]**3
           } else if (s === '#') {
               num[idx] *= -1
           } else if (s === '*') {
@@ -23,18 +23,34 @@ function solution(dartResult) {
           } 
       } else {
           if (isNaN(parseInt(prev))) {
-              if (idx === 0) {
-                  num[idx] 
-              }
+              idx++
+              num[idx] = parseInt(s)
           } else {
               prev += s
-          }
-          
-          idx = parseInt(s) - 1
-          
+              num[idx] = parseInt(prev)
+          }            
       }
       prev = s
-      console.log(s, num)
   }
   return num.reduce((prev, cur) => prev + cur)
+}
+
+// 다른 풀이
+// 정규표현식을 활용
+function solution(dartResult) {
+  const bonus = { 'S': 1, 'D': 2, 'T': 3 },
+        options = { '*': 2, '#': -1, undefined: 1 };
+
+  let darts = dartResult.match(/\d.?\D/g);
+
+  for (let i = 0; i < darts.length; i++) {
+      let split = darts[i].match(/(^\d{1,})(S|D|T)(\*|#)?/),
+          score = Math.pow(split[1], bonus[split[2]]) * options[split[3]];
+
+      if (split[3] === '*' && darts[i - 1]) darts[i - 1] *= options['*'];
+
+      darts[i] = score;
+  }
+
+  return darts.reduce((a, b) => a + b);
 }

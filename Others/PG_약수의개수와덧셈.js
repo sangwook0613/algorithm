a89006c0c48c9eff5455f3d5abf9683f151fdcca
sangwook0613 <kvmoke@gmail.com>
2021-11-22 23:52:40 +0900

    Solved problem 1122

--- Others/PG_약수의개수와덧셈.js
+++ Others/PG_약수의개수와덧셈.js
@@ -1,23 +1,37 @@
 //프로그래머스 약수의 개수와 덧셈
-
 function solution(left, right) {
-  let answer = 0
-  function getPrimeNum(n) {
-      let result = [2]
-      let chk = new Array(n+1).fill(0)
-      let i = 2
-      while (i <= n) {
-          if (chk[i] === 0) {
-              chk[i] = 1
-              result.push(i)
-              for (let a = i*2; a <= n; a += i) {
-                  chk[a] = 1
-              }
-          }
-          i++
-      }
-      return result
-  }
-  console.log(getPrimeNum(right))
-  return answer
+    let answer = 0
+    function getPrimeNum(n) {
+        let result = 2
+        for (let i = 2; i < Math.ceil(Math.sqrt(n)); i++) {
+            if (n % i === 0) {
+                result += 2
+            }
+        }
+        if (Math.ceil(Math.sqrt(n))**2 === n) {
+            result++
+        }
+        return result
+    }
+    
+    for (let a = left; a <= right; a++) {
+        let num = getPrimeNum(a)
+        answer += (num % 2 ? -a : a)
+    }
+    return answer
+}
+
+// 다른 풀이
+// 약수의 특징을 생각하자!
+// 제곱근이 정수면 약수의 개수는 홀수다!
+function solution(left, right) {
+    var answer = 0;
+    for (let i = left; i <= right; i++) {
+        if (Number.isInteger(Math.sqrt(i))) {
+            answer -= i;
+        } else {
+            answer += i;
+        }
+    }
+    return answer;
 }

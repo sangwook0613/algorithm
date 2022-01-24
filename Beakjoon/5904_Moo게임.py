1c8d7eb3c2352d919ed6a7ea8e6480264b944d0d
sangwook0613 <kvmoke@gmail.com>
2022-01-24 16:32:30 +0900

    Solved problem 0124

--- Beakjoon/5904_Moo게임.py
+++ Beakjoon/5904_Moo게임.py
@@ -1,11 +1,28 @@
 # 백준 5904 Moo 게임
-## 분할과 정복을 활용해서 풀어야 하는문제
-## 분할해서 계속 찾아나가는 과정을 간단하게 표현할 수 있는 방법을 찾아보자!
+## 분할과 정복을 활용해서 풀어야 하는 문제
 N = int(input())
-count = [3]
-curr = count[0]
-while curr < N:
-    count.append(curr*2 + len(count) + 3)
-    curr = count[-1]
-print(count)
+count = [0, 3]
+curr = 1
+while count[curr] < N:
+    count.append(count[curr]*2 + curr + 3)
+    curr += 1
 
+curr -= 1
+while True:
+    # 1. 이전까지의 합 + 1 + k+2 보다 작고 0 보다 크거나 같은지 확인
+    if 0 <= N - count[curr] < (1 + curr + 2):
+        # 1-1. 차이가 1이면 m을 출력
+        if N - count[curr] == 1:
+            print('m')
+        # 1-2. 차이가 1이 아니면 o를 출력
+        else:
+            print('o')
+        # 끝, 탈출
+        break
+    # 2. 0보다 작으면, 더 작은 값을 찾기 위해 index 수정
+    elif N - count[curr] < 0:
+        curr -= 1
+    # 3. 이전까지의 합 + 1 + k+2 보다 크다면, N에서 이전까지의 합 + 1 + k+2 을 빼고 다시 진행
+    else:
+        N -= (count[curr] + 1 + curr + 2)
+        curr -= 1
